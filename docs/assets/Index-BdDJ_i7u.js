59052cf96ac4d5ef9e204326c0c49df3b52b2c04
Foldora AI <foldora-ai@users.noreply.github.com>
2026-08-25 01:20:46 +0000

    Add Cleanora brand icons

--- docs/assets/Index-BdDJ_i7u.js
+++ docs/assets/Index-BdDJ_i7u.js
@@ -1,4 +1,4 @@
-import{j as e,m as r,A as O}from"./animation-BRsySF4U.js";import{s as P,p as U,S as _,a as V,N as T,F as L}from"./SeoHead-7L0nq058.js";import{r as c}from"./react-DK_JcHL8.js";import{c as a,t as N}from"./index-BJ99JNEO.js";import{h as R}from"./seo-C2PGEIG7.js";import"./charts-C_KtFLeW.js";/**
+import{j as e,m as r,A as O}from"./animation-BRsySF4U.js";import{s as P,p as U,S as _,a as V,N as T,F as L}from"./SeoHead-DP1eAfh6.js";import{r as c}from"./react-DK_JcHL8.js";import{c as a,t as N}from"./index-CKz1J5K3.js";import{h as R}from"./seo-FDd-cjeU.js";import"./charts-C_KtFLeW.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
